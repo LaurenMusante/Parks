aed0b37535cc767a936458d65eeaf0f81713a6d8
Lauren Musante <Lauren.Musante@gmail.com>
2019-11-01 09:10:47 -0700

    Add update/PUT and delete/DELETE functionality to parks controller

--- Parks/Controllers/ParksController.cs
+++ Parks/Controllers/ParksController.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Parks.Models;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Parks.Controllers
 {
@@ -31,11 +33,30 @@ namespace Parks.Controllers
       _db.Parks.Add(park);
       _db.SaveChanges();
     }
-    
+
     [HttpGet("{id}")]
     public ActionResult<Park> Get(int id)
     {
         return _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
     }
+
+    // PUT api/parks/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] Park park)
+    {
+        park.ParkId = id;
+        _db.Entry(park).State = EntityState.Modified;
+        _db.SaveChanges();
+    }
+
+    // DELETE api/parks/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+      var parkToDelete = _db.Parks.FirstOrDefault(entry => entry.ParkId == id);
+      _db.Parks.Remove(parkToDelete);
+      _db.SaveChanges();
+    }
+
   }
 }
